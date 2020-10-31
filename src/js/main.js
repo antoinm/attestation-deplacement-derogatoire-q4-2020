1551ed459c568736d16bfe7a2215492c3e58c2d1
Antoine <antoine.maussier@8992.fr>
2020-11-01 00:15:00 +0100

    removed useless info

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,8 @@ import './icons'
 import './check-updates'
 import { prepareForm } from './form-util'
 import { warnFacebookBrowserUserIfNecessary } from './facebook-util'
-import { addVersion } from './util'
 import { createForm } from './form'
 
 warnFacebookBrowserUserIfNecessary()
 createForm()
 prepareForm()
-addVersion(process.env.VERSION)
